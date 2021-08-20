5480f4f81fb3a1778dec1da5eaf296c328bdeffb
Gray Programmerz <hackrefisher@gmail.com>
2021-08-20 13:19:29 +0500

    Create sign.bat

---
+++ sign.bat
@@ -0,0 +1,2 @@
+cd "BlackMart"
+java.exe -jar "signer\ApkSigner.jar" sign --key "signer\apkeasytool.pk8" --cert "signer\apkeasytool.pem"  --v4-signing-enabled false --out "%cd%/build/outputs/apk/release/rafel_rat_signed.apk" "%cd%/build/outputs/apk/release/app-release-unsigned.apk"
