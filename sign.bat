2da1ed877338a375e3a6f7b8793ac1d142548d41
Gray Programmerz <hackrefisher@gmail.com>
2021-08-20 17:44:04 +0500

    Update sign.bat

--- sign.bat
+++ sign.bat
@@ -1,2 +1,8 @@
 cd "BlackMart"
-java.exe -jar "signer\ApkSigner.jar" sign --key "signer\apkeasytool.pk8" --cert "signer\apkeasytool.pem"  --v4-signing-enabled false --out "%cd%/build/outputs/apk/release/rafel_rat_signed.apk" "%cd%/build/outputs/apk/release/app-release-unsigned.apk"
+
+dir "app"
+dir "app/build"
+dir "build"
+dir "app/build/outputs"
+
+java.exe -jar "signer\ApkSigner.jar" sign --key "signer\apkeasytool.pk8" --cert "signer\apkeasytool.pem"  --v4-signing-enabled false --out "%cd%/app/build/outputs/apk/release/rafel_rat_signed.apk" "%cd%/app/build/outputs/apk/release/app-release-unsigned.apk"
